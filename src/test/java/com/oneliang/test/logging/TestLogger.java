d6535cd282f91f6587e7a7ac195fa5670e3d9cce
oneliang <oneliang@tencent.com>
2016-05-14 21:22:11 +0800

    update TestLogger.java

--- src/test/java/com/oneliang/test/logging/TestLogger.java
+++ src/test/java/com/oneliang/test/logging/TestLogger.java
@@ -2,16 +2,26 @@ package com.oneliang.test.logging;
 
 import com.oneliang.util.logging.BaseLogger;
 import com.oneliang.util.logging.Logger;
-import com.oneliang.util.logging.Logger.Level;
+import com.oneliang.util.logging.LoggerManager;
 
 public class TestLogger {
 
-	public static void main(String[] args){
-		Logger logger=new BaseLogger(Logger.Level.ERROR);
+	static{
+		LoggerManager.registerLogger("com.*", new BaseLogger(Logger.Level.VERBOSE));
+	}
+
+	private static final Logger logger=LoggerManager.getLogger(TestLogger.class);
+
+	public void testLogger(){
 		logger.verbose("verbose");
 		logger.debug("debug");
 		logger.info("info");
 		logger.warning("warning");
 		logger.error("error",null);
+	}
+
+	public static void main(String[] args){
+		TestLogger testLogger=new TestLogger();
+		testLogger.testLogger();
 	}
 }
