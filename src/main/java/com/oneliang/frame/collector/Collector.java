a017273125cb6ed81219d9f790448f91e1ce52bd
oneliang <oneliang@tencent.com>
2017-06-09 14:37:01 +0800

    optimize Collector.java

--- src/main/java/com/oneliang/frame/collector/Collector.java
+++ src/main/java/com/oneliang/frame/collector/Collector.java
@@ -1,15 +1,13 @@
 package com.oneliang.frame.collector;
 
-import java.util.List;
-
 public abstract interface Collector<FROM, DATA> {
 
     /**
      * collect
      * 
      * @param from
-     * @return List<DATA>
+     * @return DATA
      * @throws Exception
      */
-    public abstract List<DATA> collect(FROM from) throws Exception;
+    public abstract DATA collect(FROM from) throws Exception;
 }
